9013308b47e5dc7c50468b13e10ed70e079d1f56
jrest01 <julian11072000@hotmail.com>
2021-10-12 22:01:11 -0500

    Controlador listar usuarios

--- AgendaVirtual.Back/routes/users/index.js
+++ AgendaVirtual.Back/routes/users/index.js
@@ -1,25 +1,19 @@
 var express = require('express');
 var router = express.Router();
  
-//const con = require('../../services/db/conection');
+const con = require('../../services/db/conection');
 
 
 
 /* GET users listing. */
-router.get('/userList', function(req, res, next) {    
-let response=[{
-  id:1,
-  name: 'John',
-  lastName:'John',
-},
-{
-  id:2,
-  name: 'John',
-  lastName:'John',
-}];
-  res.json(response);     
+router.get('/userList', (req, res, next) => {   
+  const sqlSelect = "SELECT * FROM Usuarios"
+  con.query(sqlSelect,(err,result) =>{
+    return res.json(result);  
+  })
+  con.end();
+    
+  
 }); 
 
-
-//con.end();
 module.exports = router;
