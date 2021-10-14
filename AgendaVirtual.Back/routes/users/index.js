c88098cd5d188227d632a1e076dc45370ae82cdc
jrest01 <julian11072000@hotmail.com>
2021-10-13 21:22:21 -0500

    Controladores Usuarios

--- AgendaVirtual.Back/routes/users/index.js
+++ AgendaVirtual.Back/routes/users/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
  
 const con = require('../../services/db/conection');
 
-
-
 /* GET users listing. */
 router.get('/userList', (req, res, next) => {   
   const sqlSelect = "SELECT * FROM Usuarios"
@@ -12,8 +10,42 @@ router.get('/userList', (req, res, next) => {
     return res.json(result);  
   })
   con.end();
-    
-  
 }); 
 
+/*  users Insert. */
+router.post('/userInsert',(req,res) => {
+  const nombre = req.body.nombre;
+  const apellido = req.body.apellido;
+  const mail = req.body.mail;
+  const tel = req.body.tel;
+  const rol = req.body.rol;
+  const sqlInsert = "INSERT INTO usuarios (nombre, apellido, mail, tel, rol_id) VALUES (?,?,?,?,?)"
+  con.query(sqlInsert,[nombre,apellido,mail,tel,rol], (err,result) =>{
+    return res.json(result);
+  })
+  con.end();
+})
+
+/*  users Update. */
+router.put('/userUpdate',(req,res) => {
+  const id = req.body.id;
+  const nombre = req.body.nombre;
+  const apellido = req.body.apellido;
+  const mail = req.body.mail;
+  const tel = req.body.tel;
+  const rol = req.body.rol;
+  const sqlUpdate = "UPDATE usuarios SET nombre = ?, apellido = ?, mail = ?, tel = ?, rol_id = ? WHERE id = ?"
+  con.query(sqlUpdate, [nombre,apellido,mail,tel,rol,id], (err,result) => {
+      return res.json(result);
+    })
+})
+
+/* Desactivar usuario. */
+router.put('/userDelete',(req,res) => {
+  const id = req.body.id;
+  const sqlUpdate = "UPDATE usuarios SET activo = 'no' WHERE id = ?"
+  con.query(sqlUpdate,[id], (err,result) => {
+      return res.json(result);
+    })
+})
 module.exports = router;
